385a228118945f4ef98322daf465956d15e6974e
William Gillet <gillet.william44@gmail.com>
2022-12-27 15:16:38 +0100

    Ajout controllers, bcrypt, connect mongodb

--- back/app.js
+++ back/app.js
@@ -1,3 +1,24 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const sauceRoutes = require(".routes/sauce");
+const userRoutes= require (".routes/user");
+const helmet = require("helmet");
+const path = require("path");
 const app = express();
+
+// Mongoose connect
+mongoose.connect('mongodb+srv://jimbob:<PASSWORD>@cluster0-pme76.mongodb.net/test?retryWrites=true&w=majority',
+{ useNewUrlParser: true,
+  useUnifiedTopology: true })
+
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.setHeader('Content-Security-Policy', "default-src 'self'");
+    next();
+});
---
+++ back/middleware/multer.js
@@ -0,0 +1,23 @@
+const multer = require ("multer");
+
+// Dictionnaire extensions images
+const MIME_TYPES = {
+    'image/jpg': 'jpg',
+    'image/jpeg': 'jpg',
+    'image/png': 'png'
+  };
+
+  const strorage = multer.diskStorage({
+    // Destination images
+    destination: (req, file, callback) => {
+        callback(null, "images");
+    },
+    // Nouveau nom pour éviter les doublons
+    filename: (req, file, callback) => {
+        const name = file.originalname.split(" ").join("_");
+        const extension = MIME_TYPES[file.mimetype];
+        callback(null, name + Date.now() + "." + extension);
+    }
+  });
+
+  module.exports = multer({ storage: storage}).single("image");
